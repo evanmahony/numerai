ca737de2d638d834b8ca2c95b8e1b45e5ddc57cf
evan <evan99mahony@gmail.com>
2021-07-13 19:36:55 +0100

    Updated docker container structure.
    Split EDA from initial notebook.
    
    Introduced the wide model taking in all the possible features.
    
    Restructured files too.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM jupyter/scipy-notebook
 RUN rmdir work
-RUN pip3 install torch torchvision torchaudio numerapi xgboost
+RUN pip3 install torch torchvision torchaudio numerapi xgboost datatable
 COPY ./src/ .
 RUN python init.py
-RUN mv ./numerai_datasets/numerai_training_data.csv ./numerai_datasets/numerai_tournament_data.csv .
+RUN mkdir data train
+RUN mv ./numerai_datasets/numerai_training_data.csv ./numerai_datasets/numerai_tournament_data.csv ./data/
---
+++ src/model/wide/utils.py
@@ -0,0 +1,18 @@
+import datatable as dt
+import pandas as pd
+import torch
+
+def get_loaders(batch_size=32):
+    df = dt.fread("/home/jovyan/data/numerai_training_data.csv")
+    df = df.to_pandas()
+
+    train_df = df.sample(frac = 0.8)
+    test_df = df.drop(train_df.index)
+
+    train = torch.utils.data.TensorDataset(torch.from_numpy(train_df.iloc[:, 3:-1].values), torch.from_numpy(train_df["target"].values))
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+
+    test = torch.utils.data.TensorDataset(torch.from_numpy(test_df.iloc[:, 3:-1].values), torch.from_numpy(test_df["target"].values))
+    test_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+
+    return train_loader, test_loader
